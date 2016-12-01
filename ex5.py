a3a0723653790c3e76cdb0ddfd8a83742b1a84c8
Arielle Foldoe <afoldoe@gmail.com>
2016-12-01 15:34:18 -0800

    set up repo for python Learn Python the Hard Way's tutorials

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Arielle'
+my_age = 26
+my_height = 70
+my_weight = 200
+my_eyes = 'Brown'
+my_teeth = 'whiteish'
+my_hair = 'Dark Brown'
+
+print "Let's talk about %s." % my_name
+print "She's %f inches tall." % my_height
+print "She's %f pounts heavy." % my_weight
+print "Actually, that's not too heavy."
+print "She's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "Her teeth are usually %s depending on the coffee" % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
